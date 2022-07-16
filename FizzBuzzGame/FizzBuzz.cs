a8c860b603939057fdd701b52d06f5af0ba1ed94
ToSu95 <t.suska95@gmail.com>
2022-07-16 15:33:46 +0200

    Add project files.

---
+++ FizzBuzzGame/FizzBuzz.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    class FizzBuzz
+    {       
+
+        public int GetNumber()
+        {
+            while(true)
+            {
+                var userChoice = Console.ReadLine();
+                if(userChoice.ToUpper() == "X")
+                {
+                    Environment.Exit(0);
+                }
+
+                if(!int.TryParse(userChoice, out int number))
+                {
+                    Console.WriteLine("Podałeś nieprawidłowe dane. Spróbuj ponownie.");
+                    continue;
+                }
+                return number;
+            }
+        }
+
+        public string Result(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
+
+            else if (number % 3 == 0)
+                return "Fizz";
+
+            else if (number % 5 == 0)
+                return "Buzz";
+
+            else
+                return number.ToString();
+        }
+    }
+}
---
+++ FizzBuzzGame/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new FizzBuzz();
+
+            Console.WriteLine("Zagrajmy with FizzBuzz");          
+
+            while (true)
+            {
+                Console.WriteLine("Podaj liczbę:");
+                Console.WriteLine("Jeśli chcesz zakończyć grę naciśnij 'x'.");
+                var userNumber = game.GetNumber();
+                var gameResult = game.Result(userNumber);
+
+                Console.WriteLine(gameResult);               
+            }
+        }
+
+    }
+}
